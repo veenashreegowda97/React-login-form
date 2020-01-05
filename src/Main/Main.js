111ea2e92985c07d4bf2e153a937299b3d13155e
veenashree97 <veenashree.k@namastecredit.com>
2020-01-05 20:41:29 +0530

    main component added

---
+++ src/Main/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../Main/Main.css'
+import Form from '../Form/Form'
+
+function Main() {
+    return(
+        <div class="main-wrapper">
+            <header class="header-wrapper">
+                <div>
+                </div>
+            </header>
+            <Form/>
+        </div>
+    )
+}
+
+export default Main
